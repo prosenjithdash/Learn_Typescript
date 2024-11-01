2306801cf27773affbee7ecf901722e7f63a7e49
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2024-11-01 17:21:28 +0600

    1.4.ts file commit

---
+++ module1/src/1.4.ts
@@ -0,0 +1,24 @@
+// Basic data type
+// string
+// inpar kore nibe amader data typescript jodi amra type bole na dai, 
+let firstName = 'Pappu'; 
+
+//ar jodi type bole dai ta hole take explecive bola hoy ;
+let firstNames: string = 'Bappu'; 
+
+
+
+// number 
+let roll: number = 14749;
+
+
+// boolean 
+let isAddmin: boolean = true;
+
+
+// undefined
+let x: undefined = undefined;
+
+
+// null 
+let y: null = null;
